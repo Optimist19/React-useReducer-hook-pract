90dad983e7e3a61d2df1666d2831abb653376da9
optimist19 <oluwasegunbamidele3@gmail.com>
2023-11-11 17:19:12 +0100

    pract useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+// import DateCounter from "./DateCounter/DataCounter";
+
+import { useEffect, useReducer } from "react";
+
+
+import Header from "./React Quiz/Header";
+import Main from "./React Quiz/Main";
+import Loader from "./React Quiz/Loader";
+import Error from "./React Quiz/Error";
+import StartScreen from "./React Quiz/StartScreen";
+import Question from "./React Quiz/Question";
+import NextButton from "./React Quiz/NextButton";
+import Progress from "./React Quiz/Progress";
+import FinishScreen from "./React Quiz/FinishScreen";
+import Footer from "./React Quiz/Footer";
+import Timer from "./React Quiz/Timer";
+import Challenge from "./Challenge/Challenge";
+
+
+
+
+
+const SECS_PER_QUESTION = 30
+
+const initialState = {
+  questions: [],
+  status: "",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length + SECS_PER_QUESTION
+      };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index); //Not understood
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points //Not understood
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore
+      };
+
+    case "restart":
+      return{
+        ...initialState, question: state.question, state: "ready"
+      };
+  
+    case "tick":
+      return{
+        ...state, secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status
+      }
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  // const [state, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] =
+    useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" })); //We not interested on the payload for the dataFailed dispatch
+  }, []);
+
+  // console.log(questions)
+
+  // function ques(){
+  //   dispatch({type: "start"})
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {/* {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={ques}/>} */}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+      <br />
+      <h1>Challenge</h1>
+      <Challenge />
     </div>
   );
 }
---
+++ src/React Quiz/Question.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Options from './Options'
+
+function Question({question, dispatch, answer}) {
+	// console.log(question)
+  return (
+	<div>
+		<h4>{question.question}</h4>
+
+		<div className='options'>
+			<Options question={question} dispatch={dispatch} answer={answer} />
+		</div>
+	</div>
+  )
+}
+
+export default Question
